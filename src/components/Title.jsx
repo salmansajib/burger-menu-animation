2147781333cb741c9509a74ef7d13a03ddcd41cf
Salman-sajib <sfsajib@gmail.com>
2025-09-08 11:35:52 +0600

    edit: add gradient to the title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Title({ title = "Click on the burger menu" }) {
   return (
     <div>
-      <h1 className="py-20 font-semibold text-4xl capitalize text-center">
+      <h1 className="py-20 font-semibold text-4xl capitalize text-center bg-gradient-to-r from-purple-400 via-pink-500 to-orange-500 bg-clip-text text-transparent bg-[length:200%_100%] animate-gradient-title">
         {title}
       </h1>
     </div>
